3ccaf403cf76fdf305bdfe3976ce99a09b1a57bd
leejunnyeop <daffodil8620@naver.com>
2024-06-22 03:06:16 +0900

    [init] 초기화

---
+++ src/main/java/busim/kkilogbu/KkilogbuApplication.java
@@ -0,0 +1,13 @@
+package busim.kkilogbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkilogbuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkilogbuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/busim/kkilogbu/KkilogbuApplicationTests.java
@@ -0,0 +1,13 @@
+package busim.kkilogbu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkilogbuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
