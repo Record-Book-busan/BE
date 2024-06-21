3da1e9220057228c01c705791f0b258cc6b0d6bb
leejunnyeop <daffodil8620@naver.com>
2024-06-22 03:38:46 +0900

    [style] : swagger  yml 문서 작성

--- src/main/java/busim/kkilogbu/global/config/SwaggerConfig.java
+++ src/main/java/busim/kkilogbu/global/config/SwaggerConfig.java
@@ -8,13 +8,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
-        info =  @Info(title = "부심 API",
+        info =  @Info(title = "부심",
                 description = "끼록부: 부산 기행",
                 version = "V1")
 )
 @RequiredArgsConstructor
 @Configuration
-public class SawggerConfig {
+public class SwaggerConfig {
 
     @Bean
     public GroupedOpenApi groupedOpenApi() {
@@ -23,7 +23,7 @@ public class SawggerConfig {
         String[] paths = {"/kkilogbu/**"};
 
         return GroupedOpenApi.builder()
-                .group("마이펫 API")  // 그룹 이름을 설정한다.
+                .group("끼록부 API")  // 그룹 이름을 설정한다.
                 .pathsToMatch(paths)     // 그룹에 속하는 경로 패턴을 지정한다.
                 .build();
     }
