0825ce93b2bd1a7448efc57de41d8ac65a66fe85
kimyoobin <55120730+KimYooBin1@users.noreply.github.com>
2024-07-31 13:54:13 +0900

    [refeator]: User Entity Builder 수정

--- src/main/java/busim/kkilogbu/user/entity/User.java
+++ src/main/java/busim/kkilogbu/user/entity/User.java
@@ -1,5 +1,7 @@
 package busim.kkilogbu.user.entity;
 
+import static lombok.AccessLevel.*;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,12 +13,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table
-@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
 @Getter
 public class User {
 	@Id @GeneratedValue
@@ -34,5 +39,12 @@ public class User {
 
 	@OneToMany(mappedBy = "user")
 	private List<Bookmark> bookmarks = new ArrayList<>();
-
+	@Builder
+	public User(String username, String nickname, Long category, LocalDateTime createdAt, boolean agreePrivacy) {
+		this.username = username;
+		this.nickname = nickname;
+		this.category = category;
+		this.createdAt = createdAt;
+		this.agreePrivacy = agreePrivacy;
+	}
 }
