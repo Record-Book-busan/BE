a33889e4a9f4574762f91036a25e874ce4e4f08a
leejunnyeop <daffodil8620@naver.com>
2024-08-02 15:54:17 +0900

    [refactor] 글로벌 에러 (메세지, 상태) 정의 구현

---
+++ src/main/java/busim/kkilogbu/global/Ex/ErrorResponse.java
@@ -0,0 +1,22 @@
+package busim.kkilogbu.global.Ex;
+
+public class ErrorResponse {
+
+        private String message;
+        private int status;
+
+        public ErrorResponse(String message, int status) {
+            this.message = message;
+            this.status = status;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public int getStatus() {
+            return status;
+        }
+    }
+
+
