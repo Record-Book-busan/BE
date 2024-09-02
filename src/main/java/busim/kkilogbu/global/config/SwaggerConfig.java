b118a1fb56b6902c305e4d670b7401909069fc50
kimyoobin <55120730+KimYooBin1@users.noreply.github.com>
2024-09-03 00:41:01 +0900

    [refactor]: swagger 수정

--- src/main/java/busim/kkilogbu/global/config/SwaggerConfig.java
+++ src/main/java/busim/kkilogbu/global/config/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
     public GroupedOpenApi groupedOpenApi() {
 
         // "/v1/**" 경로에 매칭되는 API를 그룹화하여 문서화한다.
-        String[] paths = {"/kkilogbu/**"};
+        String[] paths = {"/**"};
 
         return GroupedOpenApi.builder()
                 .group("끼록부 API")  // 그룹 이름을 설정한다.
