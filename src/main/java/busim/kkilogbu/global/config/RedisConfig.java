a5ae6ba69226c7709e81e13704cc614e7a6e26e8
leejunnyeop <daffodil8620@naver.com>
2024-09-06 06:40:11 +0900

    [fix] 필드명 틀림

--- src/main/java/busim/kkilogbu/global/config/RedisConfig.java
+++ src/main/java/busim/kkilogbu/global/config/RedisConfig.java
@@ -1,36 +1,50 @@
+package busim.kkilogbu.global.config;
 
-	package busim.kkilogbu.global.config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-	import org.springframework.beans.factory.annotation.Value;
-	import org.springframework.context.annotation.Bean;
-	import org.springframework.context.annotation.Configuration;
-	import org.springframework.data.redis.connection.RedisConnectionFactory;
-	import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-	import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-	import org.springframework.data.redis.core.RedisTemplate;
-	import org.springframework.data.redis.serializer.StringRedisSerializer;
+@Configuration
+public class RedisConfig {
 
-	@Configuration
-	public class RedisConfig {
-		@Value("${spring.data.redis.host}")
-		private String host;
-		@Value("${spring.data.redis.port}")
-		private int port;
+	@Value("${spring.data.redis.host}")
+	private String host;
 
-		@Bean
-		public RedisConnectionFactory redisConnectionFactory() {
-			return new LettuceConnectionFactory (new RedisStandaloneConfiguration(host, port));
-		}
+	@Value("${spring.data.redis.port}")
+	private int port;
 
-		@Bean
-		public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-			RedisTemplate<String, String> template = new RedisTemplate<>();
-			template.setConnectionFactory(redisConnectionFactory);
-			template.setKeySerializer(new StringRedisSerializer());
-			template.setValueSerializer(new StringRedisSerializer());
-			template.setHashKeySerializer(new StringRedisSerializer());
-			template.setHashValueSerializer(new StringRedisSerializer());
-			return template;
-		}
-
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
 	}
+
+	@Bean
+	@Primary
+	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<String, String> template = new RedisTemplate<>();
+		template.setConnectionFactory(redisConnectionFactory);
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setValueSerializer(new StringRedisSerializer());
+		template.setHashKeySerializer(new StringRedisSerializer());
+		template.setHashValueSerializer(new StringRedisSerializer());
+		return template;
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> objectRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(redisConnectionFactory);
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+		template.setHashKeySerializer(new StringRedisSerializer());
+		template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+		return template;
+	}
+}
