82b37c288297cb84e90f1b91f220de3e24486c7f
leejunnyeop <daffodil8620@naver.com>
2024-09-09 02:38:41 +0900

    [refactor] cors 정책설정

--- src/main/java/busim/kkilogbu/global/config/WebConfig.java
+++ src/main/java/busim/kkilogbu/global/config/WebConfig.java
@@ -5,6 +5,7 @@ import busim.kkilogbu.place.dto.TouristCategory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -23,5 +24,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(stringToRestaurantCategoryConverter);
         registry.addConverter(stringToTouristCategoryConverter);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("*") // 모든 출처를 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 허용할 헤더
+                .allowCredentials(true); // 쿠키 허용 여부
     }
 }
