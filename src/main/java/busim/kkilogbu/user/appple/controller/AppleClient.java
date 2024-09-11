35ef7eaeb809f0cdc8da6dc0519330ccde7f728d
leejunnyeop <daffodil8620@naver.com>
2024-09-11 10:49:54 +0900

    [fix] 소셜로그인- key:value 형식으로 변경

--- src/main/java/busim/kkilogbu/user/appple/controller/AppleClient.java
+++ src/main/java/busim/kkilogbu/user/appple/controller/AppleClient.java
@@ -14,18 +14,22 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @FeignClient(name = "appleClient", url = "https://appleid.apple.com/auth", configuration = FeignConfig.class)
-@Qualifier
 public interface AppleClient {
 
     @GetMapping(value = "/keys")
     ApplePublicKeyResponse findAppleAuthPublicKeys();
 
     @PostMapping(value = "/token", consumes = "application/x-www-form-urlencoded")
-    AppleTokenResponse findAppleToken(@RequestBody AppleTokenRequest request);
+    AppleTokenResponse findAppleToken(
+            @RequestParam("client_id") String clientId,
+                                      @RequestParam("client_secret") String clientSecret,
+                                      @RequestParam("grant_type") String grantType,
+                                      @RequestParam("code") String code
+    );
 
     @PostMapping(value = "/revoke", consumes = "application/x-www-form-urlencoded")
     void revoke(AppleRevokeRequest request);
