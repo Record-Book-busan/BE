21e0e34063a5395a8dd3262cca6cb0d292c3b9e0
leejunnyeop <daffodil8620@naver.com>
2024-09-16 02:41:33 +0900

    [rm] 소셜로그인 - 테이블 삭제

--- src/main/java/busim/kkilogbu/user/repository/SocialLoginInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package busim.kkilogbu.user.repository;
-
-import busim.kkilogbu.user.entity.users.SocialLoginInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SocialLoginInfoRepository extends JpaRepository<SocialLoginInfo, Long> {
-}
