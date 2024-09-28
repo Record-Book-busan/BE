7a7b9ff12acc8d66d1bc5f3b3cc67eeb6ecf18f4
leejunnyeop <daffodil8620@naver.com>
2024-09-29 03:51:05 +0900

    [feat] 카카오소셜로그인 - 계정탈퇴

--- src/main/java/busim/kkilogbu/sociaLogin/kakao/controller/kakaoController.java
+++ src/main/java/busim/kkilogbu/sociaLogin/kakao/controller/kakaoController.java
@@ -1,12 +1,15 @@
 package busim.kkilogbu.sociaLogin.kakao.controller;
 
 
+import busim.kkilogbu.security.domain.CustomUserDetails;
 import busim.kkilogbu.sociaLogin.appple.domain.dto.SignInResponse;
 import busim.kkilogbu.sociaLogin.kakao.serivce.KakaoService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -33,4 +36,19 @@ public class kakaoController{
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("로그인 처리 중 오류가 발생했습니다.");
         }
     }
+    @DeleteMapping("/withdrawal/kakao")
+    public ResponseEntity<String> deleteKakaoUser(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        String socialUserId = customUserDetails.getUsername(); // 또는 kakaoUserId 사용
+
+        try {
+            kakaoService.deleteUserBySocialId(socialUserId);
+            return ResponseEntity.ok("카카오 계정이 성공적으로 삭제되었습니다.");
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(404).body("해당 유저를 찾을 수 없습니다.");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("계정 삭제 중 오류가 발생했습니다.");
+        }
+
+    }
+
 }
