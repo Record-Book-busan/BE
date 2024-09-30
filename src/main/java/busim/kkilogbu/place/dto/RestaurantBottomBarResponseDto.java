8354d1cfe5bff44263ad8ee46601147c45f40e36
leejunnyeop <daffodil8620@naver.com>
2024-09-30 13:32:19 +0900

    [refactor] 바텀바- request, response, mapper 분리

---
+++ src/main/java/busim/kkilogbu/place/dto/RestaurantBottomBarResponseDto.java
@@ -0,0 +1,16 @@
+package busim.kkilogbu.place.dto;
+
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class RestaurantBottomBarResponseDto {
+    private Long id;                     // 식당 ID
+    private String title;                // 제목
+    private List<String> categories;     // 카테고리 리스트
+    private String detailedInformation;  // 상세 정보
+}
