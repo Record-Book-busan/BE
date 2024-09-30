64a772f15aa6587dd899d3dd623fb9f8de985fde
kimyoobin <55120730+KimYooBin1@users.noreply.github.com>
2024-09-30 13:09:28 +0900

    [fix]: 내 기록 조회시 JSON 직렬화가 정확히 동작하지 않는 문제 해결

--- src/main/java/busim/kkilogbu/record/dto/MyRecordResponse.java
+++ src/main/java/busim/kkilogbu/record/dto/MyRecordResponse.java
@@ -4,8 +4,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
