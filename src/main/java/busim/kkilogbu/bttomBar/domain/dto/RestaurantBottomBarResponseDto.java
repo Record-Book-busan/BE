2d5a9c41dc712634c3d5dc00a40c7177a4f7a214
leejunnyeop <daffodil8620@naver.com>
2024-10-02 23:01:04 +0900

    [add] 맛집api - 이미지 추가

--- src/main/java/busim/kkilogbu/bttomBar/domain/dto/RestaurantBottomBarResponseDto.java
+++ src/main/java/busim/kkilogbu/bttomBar/domain/dto/RestaurantBottomBarResponseDto.java
@@ -13,4 +13,5 @@ public class RestaurantBottomBarResponseDto {
     private String title;                // 제목
     private List<String> categories;     // 카테고리 리스트
     private String detailedInformation;  // 상세 정보
+    private String images;
 }
