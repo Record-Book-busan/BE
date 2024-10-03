af8d8de996a88d69af2144499afc8f74bab8b39b
leejunnyeop <daffodil8620@naver.com>
2024-10-03 14:21:18 +0900

    [rn]  바텀바- url name 변경

--- src/main/java/busim/kkilogbu/bttomBar/controller/RestaurantBottomBarController.java
+++ src/main/java/busim/kkilogbu/bttomBar/controller/RestaurantBottomBarController.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/kki/place/bottom-bar")
+@RequestMapping("/kkilogbu/place/bottom-bar")
 @RequiredArgsConstructor
 public class RestaurantBottomBarController {
 
